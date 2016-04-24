2526a3120017a5f7674741728fd995a56b18835a
swwlqw <swwefj@qq.com>
2016-04-24 14:39:43 +0800

    显示名称

--- app/scripts/directives/header/header-notification/header-notification.js
+++ app/scripts/directives/header/header-notification/header-notification.js
@@ -7,12 +7,17 @@
  * # adminPosHeader
  */
 angular.module('sbAdminApp')
-	.directive('headerNotification',function(){
-		return {
-        templateUrl:'scripts/directives/header/header-notification/header-notification.html',
-        restrict: 'E',
-        replace: true,
-    	}
-	});
+    .directive('headerNotification', function () {
+        return {
+            templateUrl: 'scripts/directives/header/header-notification/header-notification.html',
+            restrict: 'E',
+            replace: true,
+            controller: function ($scope, $http) {
+                $http.get('api/user/nickname').success(function (result){
+                    $scope.nickname = result.data;
+                })
+            }
+        }
+    });
 
 
