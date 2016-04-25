fb4f1f8db66aa8b638f111fb52f641651424a823
swwlqw <swwefj@qq.com>
2016-04-25 15:35:05 +0800

    左侧终于动了

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -193,53 +193,57 @@ angular
                             ]
                         })
                     },
-                    loadMyDirectives: function ($ocLazyLoad) {
+                    loadMyDirectives:function($ocLazyLoad){
                         return $ocLazyLoad.load(
                             {
-                                name: 'sbAdminApp',
-                                files: [
+                                name:'sbAdminApp',
+                                files:[
                                     'scripts/directives/header/my_header.js',
-                                    'scripts/directives/sidebar/my_sidebar.js'
+                                    'scripts/directives/header/header-notification/header-notification.js',
+                                    'scripts/directives/sidebar/my_sidebar.js',
+                                    'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
                                 ]
                             }),
                             $ocLazyLoad.load(
                                 {
-                                    name: 'toggle-switch',
-                                    files: ["bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
+                                    name:'toggle-switch',
+                                    files:["bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
                                         "bower_components/angular-toggle-switch/angular-toggle-switch.css"
                                     ]
                                 }),
                             $ocLazyLoad.load(
                                 {
-                                    name: 'ngAnimate',
-                                    files: ['bower_components/angular-animate/angular-animate.js']
-                                }),
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngCookies',
-                                    files: ['bower_components/angular-cookies/angular-cookies.js']
-                                }),
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngResource',
-                                    files: ['bower_components/angular-resource/angular-resource.js']
-                                }),
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngSanitize',
-                                    files: ['bower_components/angular-sanitize/angular-sanitize.js']
-                                }),
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngTouch',
-                                    files: ['bower_components/angular-touch/angular-touch.js']
-                                });
+                                    name:'ngAnimate',
+                                    files:['bower_components/angular-animate/angular-animate.js']
+                                })
+                        $ocLazyLoad.load(
+                            {
+                                name:'ngCookies',
+                                files:['bower_components/angular-cookies/angular-cookies.js']
+                            })
+                        $ocLazyLoad.load(
+                            {
+                                name:'ngResource',
+                                files:['bower_components/angular-resource/angular-resource.js']
+                            })
+                        $ocLazyLoad.load(
+                            {
+                                name:'ngSanitize',
+                                files:['bower_components/angular-sanitize/angular-sanitize.js']
+                            })
+                        $ocLazyLoad.load(
+                            {
+                                name:'ngTouch',
+                                files:['bower_components/angular-touch/angular-touch.js']
+                            })
                     }
                 }
+
             })
             .state('home.apps', {
                 templateUrl: 'views/pages/apps.html',
                 url: '/apps',
+
                 resolve: {
                     loadMyDirectives: function ($ocLazyLoad) {
                         return $ocLazyLoad.load(
