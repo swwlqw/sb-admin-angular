168b441605b4ae8ec18a44c9cecd2d39f75f8103
swwlqw <swwefj@qq.com>
2016-04-26 18:56:18 +0800

    APP列表

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -4,13 +4,15 @@ angular.module('sbAdminApp')
             apps: [
                 {
                     id: 1,
+                    grant:'创建者',
                     name: 'APP1',
-                    description: '描述1',
+                    description: '描述1'
                 },
                 {
                     id: 2,
+                    grant:'管理员',
                     name: 'APP2',
-                    description: '描述2',
+                    description: '描述2'
                 }
             ]
         }
